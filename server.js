ed731e638af6d72e657668dad21b4aadde6e5c47
alsongard <alsongard@gmail.com>
2025-06-12 21:24:49 +0300

    get, get/route_params, post, update, delete all working

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware setup
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Sample in-memory products database
 let products = [
@@ -42,7 +42,7 @@ let products = [
 
 // Root route
 app.get('/', (req, res) => {
-  res.send('Welcome to the Product API! Go to /api/products to see all products.');
+  return res.send('<div><h1> Hello World </h1> <p> Welcome to the Product API! Go to /api/products to see all products.</p></div> ');
 });
 
 // TODO: Implement the following routes:
@@ -54,9 +54,102 @@ app.get('/', (req, res) => {
 
 // Example route implementation for GET /api/products
 app.get('/api/products', (req, res) => {
-  res.json(products);
+  res.status(200).json({success: true, data: products});
 });
 
+app.get('/api/products/:id', (req,res)=>{
+  // get route parameters from request
+  console.log(req.params)
+  const {id} = req.params; // the param must be exact to whats given in the id
+  // by default the id param is of type string
+  console.log(id)
+  console.log(typeof(id));
+  // use find to get the product with the id : javascript array method
+  // find method returns the first product that matches with the id:(unique for each product)
+  // if not returns undefined 
+  const foundProduct = products.find((productItem)=>{return productItem.id == id})
+  if (foundProduct)
+  {
+    res.status(200).json({success: true, data: foundProduct})
+  }
+  else
+  {
+    res.status(200).json({success: false, msg: `No product was found with the id : ${id}`})
+  }
+})
+
+app.post("/api/products", (req,res)=>{
+  const { id, name, description, price, category, instock} =  req.body;
+  // products is an array : modifiable: let
+  // how to append data to javascript array : push()
+  products.push({id, name, description, price,  category, instock});
+  res.json({success: true, msg: "New Product has been Added" , data: products});
+  console.log(products);
+})
+
+
+app.put("/api/products/:id", (req, res)=>{
+  // get product detail to change from body
+  const {name, description, price, category, instock} =  req.body;
+  
+  // get the route parameter === for a given product || update product based on the id
+  const {id} = req.params;
+  // find the product
+  const foundProduct = products.find((productItem)=>{return productItem.id == id});
+  if (foundProduct)
+  {
+    // check if req.body is undefined
+  
+    if (name)
+    {
+      foundProduct.name = name;
+    }
+    else if (description)
+    {
+      foundProduct.description = description;
+    }
+    else if (price)
+    {
+      foundProduct.price = price;
+    }
+    else if (category)
+    {
+      foundProduct.category = category;
+    }
+    else if (instock)
+    {
+      foundProduct.inStock = instock;
+    }
+    else if (!name || !description || !price || !category || !instock)
+    {
+      return res.status(200).send("<p>No new details for the product ${id} was given</p>")
+    }
+  }
+  else
+  {
+    return res.status(200).json({success: false, msg: `No product with id : ${id} found! Try with another id`})
+  }
+  console.log(products)
+  return res.json({success: true, msg:`Product with id : ${id} has been updated`, data: products})
+})
+
+app.delete("/api/products/:id", (req, res)=>{
+  // get parameter
+  const {id} = req.params;
+
+   // we will use filter : returns a new array that meet the given condition in the callback function
+  const foundProducts = products.filter((productItem)=>{return productItem.id != id});
+
+  products  = foundProducts;
+  if (foundProducts)
+  {
+    return res.status(200).json({success: true, msg: `Product with id: ${id} has been deleted`, data: products});
+  }
+  else
+  {
+    return res.status(200).json({success: false, msg:`No product with the ${id} has been found`});
+  }
+})
 // TODO: Implement custom middleware for:
 // - Request logging
 // - Authentication
@@ -68,4 +161,4 @@ app.listen(PORT, () => {
 });
 
 // Export the app for testing purposes
-module.exports = app; 
+// module.exports = app; 
